ead247e97328e0cb606624b250d4b0f9ea19dd52
easonhan <nbkhic@qq.com>
2013-04-15 14:16:05 +0800

    add google search example

---
+++ lib/lazyman/generators/lazyman/app/cases/examples/google_example_spec.rb
@@ -0,0 +1,16 @@
+#encoding: utf-8
+require File.expand_path 'app/cases/spec_helper'
+
+describe 'Google example show you how to use lazyman', :google do
+
+	before :all do
+		@keyword = 'watir-webdriver'
+	end
+
+	it 'should have correct search result' do
+		google_page = $navi.goto_baidu_page
+		result_page = google_page.search_for @keyword
+		result_page.first_result.should eq 'Watir WebDriver | the most elegant way to use webdriver with ruby'
+	end
+
+end
